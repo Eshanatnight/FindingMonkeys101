f13569e79adcac88adf5ec7483dbff2c91e5d6c1
Eshan Chatterjee <eshan2001221@gmail.com>
2022-09-26 17:06:05 +0530

    Initial Commit

---
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    List<Monkey> monkeyList = new();
+    HttpClient httpClient;
+
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+
+        var url = "https://www.montemagno.com/monkeys.json";
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        /*
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize<List<Monkey>>(contents);
+        */
+
+        return monkeyList;
+    }
+}
